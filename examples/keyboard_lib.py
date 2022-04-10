2b5490527bd738d8f6b7c2e84130359398975f9a
kensoidev <65781959+kensoi@users.noreply.github.com>
2022-04-10 23:36:54 +0500

    Create keyboard_lib.py

---
+++ examples/keyboard_lib.py
@@ -0,0 +1,32 @@
+"""
+Copyright 2022 kensoi
+"""
+
+from vkbotkit.objects import decorators, filters, enums, library_module
+from vkbotkit.objects.keyboard import *
+
+
+class basic_lib(library_module):
+    async def start(self, toolkit):
+        self.keyboardTest = keyboard(one_time = False, inline = True)
+        self.keyboardTest2 = keyboard(one_time = False, inline = True)
+
+        self.keyboardTest.add_button("канари помощь", keyboardcolor.PRIMARY)
+        self.keyboardTest.add_line()
+        self.keyboardTest.add_button("Test1")
+        self.keyboardTest.add_button("Test2")
+        self.keyboardTest.add_line()
+        self.keyboardTest.add_button("Test3")
+        
+        self.keyboardTest2.add_button("тесто", keyboardcolor.PRIMARY)
+        self.keyboardTest2.add_line()
+        self.keyboardTest2.add_location_button()
+
+    @decorators.callback(filters.whichUpdate({enums.events.message_new,}))
+    async def send_hello(self, package):
+        await package.toolkit.api.messages.send(
+            random_id = package.toolkit.gen_random(),
+            peer_id = package.peer_id,
+            message = "Пример клавиатуры",
+            keyboard = self.keyboardTest.get_keyboard()
+        )
