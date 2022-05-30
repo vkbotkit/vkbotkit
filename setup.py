ca9557bb6b2f8a98d856b90c4db679d6c474897a
kensoi <65781959+kensoi@users.noreply.github.com>
2022-05-30 08:33:09 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,20 +7,23 @@ setuptools.setup(
     version="1.0a7",
     author="kensoi",
     author_email="kensoidev@gmail.com",
-    description="Simple Bot framework for VK Communities",
+    description=(
+        'асинхронная библиотека с инструментами для разработки ботов ВКонтакте'
+    ),
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://www.dshdev.ru/vkbotkit",
+    url="https://kensoi.github.io/vkbotkit",
     project_urls={
+        "Releases": "https://github.com/kensoi/vkbotkit/releases",
         "Bug Tracker": "https://github.com/kensoi/vkbotkit/issues",
-        "Bot Template": "https://github.com/kensoi/vkbotkit_template/",
+        "Bot Template": "https://github.com/kensoi/vkbotkit_app/",
     },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    packages = setuptools.find_packages(include=("vkbotkit",)),
+    packages = setuptools.find_packages(),
     install_requires = ["aiohttp", "six"],
     python_requires = '>=3.7',
 )
